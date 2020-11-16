ad278d8e24f83a4fe16a08bfd1a5a749b0984d40
quanghai29 <36925426+quanghai29@users.noreply.github.com>
2020-11-16 20:56:40 +0700

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb284c6b83a2f001f2df9a0",
+				"5fb284c6b83a2f001f2df9a2"
+			]
+		},
+		"5fb284c6b83a2f001f2df9a0": {
+			"id": "5fb284c6b83a2f001f2df9a0",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb284c6b83a2f001f2df9a2": {
+			"id": "5fb284c6b83a2f001f2df9a2",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
